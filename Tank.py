2e223bca80a3d6784eb54008f7f1172a44ecbe1f
CallumCodekids <callum@codekids.com.au>
2023-10-14 13:15:36 +0800

    added rotation

--- Tank.py
+++ Tank.py
@@ -9,6 +9,26 @@ class Tank:
         self.height = height
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image,(self.width,self.height))
+        self.speed = 0
+        self.angle = 0
         
     def display(self,screen):
-        screen.blit(self.image,(self.x,self.y))
+        screen.blit(pygame.transform.rotate(self.image,self.angle),
+                    (self.x,self.y))
+        
+    def update(self):
+        self.rotate()
+        self.back_and_forward()
+        
+    def rotate(self):
+        self.angle = self.angle +1
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            print("a was pressed")
+        if keys[pygame.K_d]:
+            print("d was pressed")
+
+        
+    def back_and_forward(self):
+        pass
+        
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pygame
 from Tank import Tank
 
 pygame.init()
-
+clock = pygame.time.Clock()
 screen_width = 1000
 screen_height = 600
 tank = Tank(400,200,40,60,"player_tank.png")
+fps = 60
 screen = pygame.display.set_mode((screen_width,screen_height))
 bg = pygame.image.load("background.jpg")
 bg= pygame.transform.scale(bg,(screen_width,screen_height))
@@ -17,7 +18,8 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     screen.blit(bg,(0,0))
+    tank.update()
     tank.display(screen)
     pygame.display.flip()
-            
+    clock.tick(fps)
 pygame.quit()
