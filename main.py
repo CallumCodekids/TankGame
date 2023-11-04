d36a47427d795aa75d461944b312253aaead75fc
Justafloopa <147900756+Justafloopa@users.noreply.github.com>
2023-11-04 13:12:20 +0800

    memememem am borders
    mama

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ pygame.init()
 clock = pygame.time.Clock()
 screen_width = 1000
 screen_height = 600
-tank = Tank(400,200,60,60,"player_tank.png", 5)
 fps = 60
 screen = pygame.display.set_mode((screen_width,screen_height))
+tank = Tank(400,200,60,60,"player_tank.png", 5, screen)
 bg = pygame.image.load("background.jpg")
 bg= pygame.transform.scale(bg,(screen_width,screen_height))
 
@@ -19,7 +19,7 @@ while run:
             run = False
     screen.blit(bg,(0,0))
     tank.update()
-    tank.display(screen)
+    tank.display()
     pygame.display.flip()
     clock.tick(fps)
 pygame.quit()
